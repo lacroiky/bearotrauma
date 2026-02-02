a698be78f5e2e3b02beb1c409b4289395703cfe0
lacroiky <lacroiky@github>
2026-02-02 08:58:49 -0500

    add installer

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+LOCAL_MOD_DIR="E:/SteamLibrary/steamapps/common/Barotrauma/LocalMods"
+REPO_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+REPO_MOD_DIR="${REPO_DIR}/mods"
+
+testmod () {
+    if [ -z "$1" ];
+    then
+        echo "testing all mods in mods directory"
+        for dir in "${REPO_MOD_DIR}"/*/
+        do 
+            DIR_PATH=${dir%*/}
+            DIR_NAME=$(basename "${DIR_PATH}")
+            echo "making clean install of mod: ${DIR_NAME}"
+            rm -rf "${LOCAL_MOD_DIR}/${DIR_NAME}"
+            cp -r  "${DIR_PATH}" "${LOCAL_MOD_DIR}"
+        done
+    fi
+}
+
+testmod
